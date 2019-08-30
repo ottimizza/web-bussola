f940e4389c1596602d6d5069dea9adf741583496
zuanz <ruanrgm@gmail.com>
2019-08-30 16:34:54 -0300

    fix es6 for real

--- server.js
+++ server.js
@@ -26,16 +26,16 @@ const envConfigFile = `
 }
 `;
 
-writeFile(targetPath, envConfigFile, function(err) {
+fs.writeFile(targetPath, envConfigFile, function(err) {
 	if (err) {
 		console.log('ERRO!\n' + err);
 	}
 });
 
-app.use(static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('*', function(req, res) {
-	res.sendFile(join(__dirname + '/dist/index.html'));
+	res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
