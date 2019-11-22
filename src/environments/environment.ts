7dc09b8b04ae780234e5dff9ece140521a359109
zuanz <ruanrgm@gmail.com>
2019-11-22 09:18:08 -0300

    environment to staging

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
-	production: true,
-	oauthBaseUrl: 'https://development-oauth-server.herokuapp.com',
-	appApi: 'https://development-api-bussolapwa.ottimizza.com.br',
+	production: false,
+	oauthBaseUrl: 'https://staging-oauth.ottimizza.com.br',
+	appApi: 'https://staging-api-bussolapwa.ottimizza.com.br',
 	storageUrl: 'https://s4.ottimizzacontabil.com:55325',
-	oauthClientId: 'be511540971aa9a0ea3c',
-	applicationId: 'development-bussola-contabil',
+	oauthClientId: 'bussola-contabil-client',
+	applicationId: 'staging-bussola-contabil',
 	firebase: {
 		apiKey: 'your apikey',
 		authDomain: 'your authDomain',
