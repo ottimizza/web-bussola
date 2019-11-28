df6636f41d6a753469e36f532d330a80cfd70421
zuanz <ruanrgm@gmail.com>
2019-11-28 15:20:36 -0300

    prod

--- src/config-env.js
+++ src/config-env.js
@@ -17,7 +17,7 @@ const envConfigFile = `export const environment = {
 };
 `;
 
-fs.writeFile(`./src/environments/environment.ts`, envConfigFile, err => {
+fs.writeFile(`./src/environments/environment.prod.ts`, envConfigFile, err => {
 	if (err) {
 		console.log(err);
 	}
