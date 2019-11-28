5fdbd51259b80cd0386973ce644c2dad865b0a4f
zuanz <ruanrgm@gmail.com>
2019-11-28 09:59:40 -0300

    environment vars #4 try

---
+++ config-env.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+// This is good for local dev environments, when it's better to
+// store a projects environment variables in a .gitignore'd file
+require('dotenv').config();
+
+const envConfigFile = `export const environment = {
+	production: true
+   apiBaseUrl: '${process.env.API_BASE_URL}',
+   apiUrl: '${process.env.API_URL}',
+   appName: '${process.env.APP_NAME}',
+   awsPubKey: '${process.env.AWSKEY}',
+   nodeEnv: '${process.env.NODE_ENV}',
+   production: '${process.env.PRODUCTION}'
+};
+`;
+
+const environment = getEnvironmentVariable('ENVIRONMENT');
+
+console.log(`
+  ENVIRONMENT -> ${process.env.ENVIRONMENT}
+  ---
+  ${envConfigFile}
+`);
+
+fs.writeFile(`./src/environments/environment.prod.ts`, envConfigFile, err => {
+	if (err) {
+		console.log(err);
+	}
+});
