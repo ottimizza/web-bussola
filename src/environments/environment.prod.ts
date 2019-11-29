da56af857706f286ef1f2a43dd0c40394d531d59
zuanz <ruanrgm@gmail.com>
2019-11-29 11:26:46 -0300

    prod json.parse

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,16 +1,1 @@
-export const environment = {
-	production: true,
-	oauthBaseUrl: 'https://oauth.ottimizza.com.br',
-	appApi: 'https://api-bussolapwa.ottimizza.com.br',
-	storageUrl: 'https://s4.ottimizzacontabil.com:55325',
-	oauthClientId: 'ab81522e9491661af9b9',
-	applicationId: 'bussola-contabil',
-	firebase: {
-		apiKey: 'your apikey',
-		authDomain: 'your authDomain',
-		databaseURL: 'your databaseUrl',
-		projectId: 'your projectId',
-		storageBucket: 'your storageBucket',
-		messagingSenderId: 'your messagingSenderId'
-	}
-};
+export const environment = JSON.parse(require('../assets/data/appConfig.json'));
