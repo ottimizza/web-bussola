5034bb1ba5876c3bb123e93422289d34300e05d4
zuanz <ruanrgm@gmail.com>
2019-11-29 13:36:21 -0300

    rollback no environment

--- src/config-env.js
+++ src/config-env.js
@@ -1,17 +1,6 @@
-const express = require('express');
-
-const app = express();
-
-const path = require('path');
-
-const compression = require('compression');
-
-const zlib = require('zlib');
-
 const fs = require('fs');
 // This is good for local dev environments, when it's better to
 // store a projects environment variables in a .gitignore'd file
-
 require('dotenv').config();
 
 function getEnvironmentVariable(key, _default = '') {
@@ -32,19 +21,4 @@ fs.writeFile(`./src/environments/environment.ts`, envConfigFile, err => {
 	if (err) {
 		console.log(err);
 	}
-});
-
-app.use(express.static(__dirname + '/dist/bussola-pwa'));
-
-app.disable('etag');
-
-app.use(compression(zlib.Z_BEST_COMPRESSION));
-
-app.listen(process.env.PORT || 4200);
-
-// redirect traffic to index.html
-
-app.get('*', function(req, res) {
-	const index = __dirname + '/dist/bussola-pwa/index.html';
-	res.sendFile(path.join(index));
 });
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,2 +1,16 @@
-declare var require: any;
-export const environment = JSON.parse(require('../assets/data/appConfig.json'));
+export const environment = {
+	production: false,
+	oauthBaseUrl: 'https://oauth.ottimizza.com.br',
+	appApi: 'https://api-bussolapwa.ottimizza.com.br',
+	storageUrl: 'https://s4.ottimizzacontabil.com:55325',
+	oauthClientId: 'ab81522e9491661af9b9',
+	applicationId: 'bussola-contabil',
+	firebase: {
+		apiKey: 'your apikey',
+		authDomain: 'your authDomain',
+		databaseURL: 'your databaseUrl',
+		projectId: 'your projectId',
+		storageBucket: 'your storageBucket',
+		messagingSenderId: 'your messagingSenderId'
+	}
+};
